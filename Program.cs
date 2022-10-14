d6e8dc44f94630141cd214fb5435cb0d7cbf5f90
ZombiDev <TheWatchingDog@yahoo.de>
2022-10-14 17:48:19 +0200

    Working Paddles

--- Program.cs
+++ Program.cs
@@ -1,31 +1,73 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.KeyboardKey;
+using static Raylib_cs.Color;
 
 namespace PongGame
 {
-    static class Program
+    public class Program
     {
-        public static void Main()
+        public static int Main()
         {
+            // Initialization
+            //-------------------------------------------------------------
             const int screenWidth = 800;
             const int screenHeight = 400;
+            const int screenOneFourth = (int) (screenWidth * 0.25);
+            const int screenThreeFourth = (int) (screenWidth * 0.75);;
             const string title = "Pong";
-            int scoreA, scoreB;
-            Raylib.InitWindow(screenWidth, screenHeight, title);
+            
+            InitWindow(screenWidth, screenHeight, title);
 
+            Vector2 ballPosition = new Vector2((float) screenWidth / 2, (float) screenHeight / 2);
+
+            int playerPosiotionA = 20;
+            int playerPosiotionB = 20;
+            int paddleHeight = 100;
+            
+            int scoreA, scoreB;
             scoreA = 0;
             scoreB = 0;
-
-            while (!Raylib.WindowShouldClose())
+            
+            SetTargetFPS(60);
+            //------------------------------------------------------------
+            
+            
+            // Main game loop
+            while (!WindowShouldClose())
             {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.WHITE);
-                Raylib.DrawText(scoreA.ToString(), (screenWidth / 2) - 10, 12, 20, Color.BLACK);
-                Raylib.DrawText(scoreB.ToString(), (screenWidth / 2) + 10, 12, 20, Color.BLACK);
-               
-                Raylib.EndDrawing();
+                // Update
+                //--------------------------------------------------------
+                if (IsKeyDown(KEY_S) && (playerPosiotionA + paddleHeight) < screenHeight)
+                    playerPosiotionA += 2;
+                if (IsKeyDown(KEY_W) && playerPosiotionA > 0)
+                    playerPosiotionA -= 2;
+
+                if (IsKeyDown(KEY_DOWN) && (playerPosiotionB + paddleHeight) < screenHeight)
+                    playerPosiotionB += 2;
+                if (IsKeyDown(KEY_UP) && playerPosiotionB  > 0)
+                    playerPosiotionB -= 2;
+                    //--------------------------------------------------------
                 
+                // Draw
+                //--------------------------------------------------------
+                BeginDrawing();
+                ClearBackground(Color.BLACK);
+                DrawText(scoreA.ToString(), screenOneFourth, 12, 50, Color.WHITE);
+                DrawText(scoreB.ToString(), screenThreeFourth, 12, 50, Color.WHITE);
+                
+                DrawRectangle(10, playerPosiotionA, 10, paddleHeight, Color.WHITE);                              // Player A
+                DrawRectangle(780, playerPosiotionB, 10, paddleHeight, Color.WHITE);                             // Player B
+
+                DrawCircleV(ballPosition, 10, MAROON);
+                
+                EndDrawing();
+                //----------------------------------------------------------
             }
             Raylib.CloseWindow();
+            
+            return 0;
         }
     }
 }
